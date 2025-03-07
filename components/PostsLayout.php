a0ff1386a4e3e4b30a496e5a7012e8381781d627
akeliansevag <akelian.sevag@gmail.com>
2025-03-07 15:08:47 +0200

    .

--- components/PostsLayout.php
+++ components/PostsLayout.php
@@ -17,7 +17,7 @@ $posts = get_posts($arguments);
     <div>
         <div class="flex justify-between items-center">
             <h2 class="section-title"><?= $category->name; ?></h2>
-            <a class="underline" href="<?= get_category_link($category) ?>">MORE</a>
+            <a class="underline" href="<?= get_category_link($category) ?>"><?= _e('MORE', 'thespeech'); ?></a>
         </div>
 
         <div class="flex max-lg:flex-col max-lg:gap-10 lg:gap-5">
