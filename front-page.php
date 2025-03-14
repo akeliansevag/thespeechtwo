18c8fd2be928715b4b416b32c84f640dc3a17a90
akeliansevag <akelian.sevag@gmail.com>
2025-03-14 14:52:15 +0200

    .

--- front-page.php
+++ front-page.php
@@ -15,12 +15,12 @@
             </h2>
             <div class="ltr:lg:pl-4 rtl:lg:pr-4">
                 <div class="flex items-center justify-between cursor-pointer group" id="who-we-are-handle">
-                    <h3 class="group-hover:text-primary ltr:font-englishTitles font-bold">
+                    <h3 class="group-hover:text-secondary ltr:font-englishTitles font-bold">
                         <?= get_field('about_subtitle'); ?>
                     </h3>
-                    <div class="hover:opacity-40 cursor-pointer transition-all" id="who-we-are-arrow">
+                    <div class="group cursor-pointer transition-all" id="who-we-are-arrow">
                         <svg width="32" height="34" viewBox="0 0 32 34" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M17 1C17 0.447715 16.5523 -2.41411e-08 16 0C15.4477 2.41411e-08 15 0.447715 15 1L17 1ZM15.2929 33.7071C15.6834 34.0976 16.3166 34.0976 16.7071 33.7071L23.0711 27.3431C23.4616 26.9526 23.4616 26.3195 23.0711 25.9289C22.6805 25.5384 22.0474 25.5384 21.6569 25.9289L16 31.5858L10.3431 25.9289C9.95262 25.5384 9.31946 25.5384 8.92893 25.9289C8.53841 26.3195 8.53841 26.9526 8.92893 27.3431L15.2929 33.7071ZM15 1L15 33L17 33L17 1L15 1Z" fill="black" />
+                            <path d="M17 1C17 0.447715 16.5523 -2.41411e-08 16 0C15.4477 2.41411e-08 15 0.447715 15 1L17 1ZM15.2929 33.7071C15.6834 34.0976 16.3166 34.0976 16.7071 33.7071L23.0711 27.3431C23.4616 26.9526 23.4616 26.3195 23.0711 25.9289C22.6805 25.5384 22.0474 25.5384 21.6569 25.9289L16 31.5858L10.3431 25.9289C9.95262 25.5384 9.31946 25.5384 8.92893 25.9289C8.53841 26.3195 8.53841 26.9526 8.92893 27.3431L15.2929 33.7071ZM15 1L15 33L17 33L17 1L15 1Z" fill="black" class="group-hover:fill-secondary" />
                         </svg>
                     </div>
                 </div>
