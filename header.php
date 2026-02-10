87c6b51396c3cad7516526f3ae341465f13fac13
akeliansevag <akelian.sevag@gmail.com>
2026-02-10 13:59:55 +0200

    .

--- header.php
+++ header.php
@@ -11,6 +11,8 @@
 	<link href="https://fonts.googleapis.com/css2?family=Readex+Pro:wght@160..700&display=swap" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css2?family=Spectral:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9520398356263727"
+		crossorigin="anonymous"></script>
 	<?php wp_head(); ?>
 </head>
 
